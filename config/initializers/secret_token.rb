478a491a0eb2be5853db93b8757d184200952228
Toru KAWAMURA <tkawa@4bit.net>
2012-09-29 17:56:41 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DominionCards::Application.config.secret_token = '0c9adf8089ecbade574966e834c7a3ae2173fa33a94a60eafcaa1bc0aac4bc06c3a840d5ee36fb42513dcc9e69fd5865a66cf51a6c309a4c651ab6939035f05f'
