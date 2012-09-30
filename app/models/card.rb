7311a38ac18641475019e05506083ef707d4df66
Toru KAWAMURA <tkawa@4bit.net>
2012-09-30 15:58:14 +0900

    Make card lists simple

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,8 @@
 class Card < ActiveRecord::Base
   include Enumerize
-  enumerize :set, :in => [:base, :intrigue, :seaside, :alchemy, :prosperity, :cornucopia, :hinterlands, :promo]
+  enumerize :set, :in => [:base, :intrigue, :seaside, :alchemy, :prosperity, :cornucopia, :hinterlands, :dark_ages, :promo]
+
+  def cost_p
+    "#{cost}#{'p' * potion.to_i}"
+  end
 end
