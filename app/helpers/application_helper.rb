2e96a72750b3167450ecb8772b61e801d82644f8
Toru KAWAMURA <tkawa@4bit.net>
2012-10-13 09:02:24 +0900

    Translate tweet text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# -*- encoding: utf-8 -*-
 module ApplicationHelper
+  def picked_tweet_text(cards)
+    connector = I18n.t('picked_tweet.cards_connector', default: [:'support.array.words_connector', ', '])
+    I18n.t('picked_tweet.format', cards: cards.map(&:name_t).join(connector).truncate(110, omission: '…'))
+  end
 end
