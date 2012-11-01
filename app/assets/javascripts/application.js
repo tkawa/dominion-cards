5b73d51533524cea845136d6ac4f8919bfcc067a
Toru KAWAMURA <tkawa@4bit.net>
2012-11-01 12:27:15 +0900

    Fix that cannot click dropdown links on iOS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,8 @@
 //= require bootstrap-collapse
 //= require bootstrap-dropdown
 //= require_tree .
+
+// dropdown patch for iOS
+$('body').on('touchstart.dropdown.data-api', '.dropdown-menu', function (e) { e.stopPropagation() });
+// Twitter widgets
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
